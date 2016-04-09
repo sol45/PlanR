0b63aecc07eeee15258a080a858b47419ccf5a55
sol45 <sol@lumidium.com>
2016-04-09 16:07:29 +0100

    made it better

--- static/javascript.js
+++ static/javascript.js
@@ -1,13 +1,21 @@
 //object.onload = runpage.call
 
+
 function runpage() {
 	console.log("loaded");
+	var form1 = document.getElementById("form1");
+	var form2 = document.getElementById("form2");
+	form2.style.display = 'none';
 }
 
 function week1() {
 	console.log("week1");
+	form1.style.display = 'block';
+	form2.style.display = 'none';
 }
 
 function week2() {
 	console.log("week2")
+	form1.style.display = 'none';
+	form2.style.display = 'block';
 }
